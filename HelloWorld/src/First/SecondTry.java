01ec9e1a3a779f167cd7ea574053158e7078ff42
agarwal.aashish1811 <agarwal.aashish1811@gmail.com>
2023-05-27 18:05:44 +0530

    Second Trya

--- HelloWorld/src/First/SecondTry.java
+++ HelloWorld/src/First/SecondTry.java
@@ -5,6 +5,7 @@ public class SecondTry {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Jai Shri Shyam");
+		System.out.println("Jai Shri Shyam2");
 
 	}
 
